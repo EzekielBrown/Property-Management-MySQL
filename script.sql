1cc0d742ee897cc3cdbba01726127200d4433938
Zek Brown <zek-brown@live.nmit.ac.nz>
2022-10-18 12:51:01 +1300

    added questions

---
+++ script.sql
@@ -0,0 +1,19 @@
+-- A list of all properties, including the property address, owner and agent responsible for each property ordered alphabetically by city.
+
+--  A list of the properties available to rent or lease, including the property lease/rent amount and period, the property address and agents responsible for each property ordered low to high by rental/lease price.
+
+-- A list of staff including their name, phone, email, position (i.e. agent, accounts etc..) and their manager filtered by branch ordered alphabetically by the agent's name.
+
+-- A list of current owners and tenants, including their contact details and a field indicating if they are an owner, or tenant ordered alphabetically by name.
+
+-- A list of clients that have viewed properties in the last four weeks, including the client's name, contact details and the number of properties they have visited, filtered by the branch they are registered with ordered high to low by the visits.
+
+-- A list of upcoming adverts placed, including the property, date and the publication where the advert has been placed ordered by date showing the newest first.
+
+-- A query to update all properties managed by an agent to another agent.
+
+-- A query to delete all the client property viewings older than 12 months.
+
+-- A query to delete all properties for a specific owner.
+
+-- Write your own query relevant to the scenario and include a written description of what the query is aiming to do.
